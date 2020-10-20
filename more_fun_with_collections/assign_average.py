a8b029924df00bc287ded027a8286e4fe4fa006b
atilley7 <averytilley7@gmail.com>
2020-10-19 21:04:21 -0500

    passed test 2

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -1,7 +1,9 @@
 def switch_average(key_val):
     switch = {
         'A': "one",
-        'a': "one"
+        'a': "one",
+        'B': "two",
+        'b': "two"
 
     }
     return switch.get(key_val)
