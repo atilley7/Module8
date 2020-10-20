5d0905f4680121fea094ecbab883ca2711315a86
atilley7 <averytilley7@gmail.com>
2020-10-19 21:11:44 -0500

    docstring

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -1,16 +1,20 @@
+"""
+Avery Tilley
+10/19/20
+Python switch case program
+"""
 def switch_average(key_val):
     switch = {
         'A': "one",
         'a': "one",
         'B': "two",
         'b': "two",
-        'C':"three",
-        'c':"three",
-        'D':"four",
-        'd':"four",
-        'E':"five",
-        'e':"five"
-
+        'C': "three",
+        'c': "three",
+        'D': "four",
+        'd': "four",
+        'E': "five",
+        'e': "five"
 
     }
     return switch.get(key_val, "invalid key")
